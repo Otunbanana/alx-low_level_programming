e5d923d03243d4833d46295d265d917a9f19ad4d
Musa Otun <otunmusa@gmail.com>
2023-03-02 15:33:07 -0800

    commit at 2023-03-02_15:33:07

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,28 @@
 
 int _strcmp(const char *s1, const char *s2)
 {
-while (*s1 && (*s1 == *s2))
+int i;
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
-s1++;
-s2++;
+if (s1[i] < s2[i])
+{
+return -1;
 }
-
-return (*s1 - *s2);
+else if (s1[i] > s2[i])
+{
+return 1;
+}
+}
+if (s1[i] == '\0' && s2[i] == '\0')
+{
+return 0;
+}
+else if (s1[i] == '\0')
+{
+return -1;
+}
+else
+{
+return 1;
+}
 }
