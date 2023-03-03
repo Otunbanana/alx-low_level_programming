4523497a2e0ac94f2759574a07cc6d4cab9a7574
Musa Otun <otunmusa@gmail.com>
2023-03-02 17:15:12 -0800

    commit at 2023-03-02_17:15:12

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,19 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
-* print_number - Prints an integer using _putchar function
-* @n: The integer to print
+int main(void)
+{
+int n;
+int w[5];
+int *d;
+
+w[2] = 1024;
+d = &n;
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
 */
-void print_number(int n)
-{
-if (n < 0)
-{
-_putchar(45);
-n *= -1;
-}
-
-if (n / 10 != 0)
-print_number(n / 10);
-
-_putchar(n % 10 + '0');
+*(d + 5) = 98;
+/* ...so that this prints 98\n */
+printf("w[2] = %d\n", w[2]);
+return (0);
 }
