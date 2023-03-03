46a1ce50b1c563b64b25606bca61253194d3a4c5
Musa Otun <otunmusa@gmail.com>
2023-03-02 16:33:41 -0800

    commit at 2023-03-02_16:33:41

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,21 +4,35 @@
 * print_number - prints an integer
 * @n: the integer to print
 */
+
 void print_number(int n)
 {
-
+int num_digits = 0;
+int temp = n;
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+temp = -n;
 }
-
-if (n / 10 == 0)
+while (temp != 0)
 {
-_putchar(n % 10 + '0');
-return;
+num_digits++;
+temp /= 10;
 }
-
-print_number(n / 10);
-_putchar(n % 10 + '0');
+while (num_digits > 0)
+{
+int divisor = 1;
+for (int i = 1; i < num_digits; i++)
+{
+divisor *= 10;
+}
+int digit = n / divisor;
+_putchar(digit + '0');
+n %= divisor;
+num_digits--;
+}
+if (n == 0)
+{
+_putchar('0');
+}
 }
