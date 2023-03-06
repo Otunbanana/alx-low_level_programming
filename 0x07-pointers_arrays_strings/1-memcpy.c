46c914593c6bdbacad2046fe32c468f0ac44824a
Musa Otun <otunmusa@gmail.com>
2023-03-06 13:43:31 -0800

    commit at 2023-03-06_13:43:31

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,8 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 char *dest_ptr = dest;
 char *src_ptr = src;
+unsigned int i;
 
-for (unsigned int i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
 *dest_ptr++ = *src_ptr++;
 }
