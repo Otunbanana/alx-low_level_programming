863ac9d163b9360e7a97b80a36edc1dad670229c
Musa Otun <otunmusa@gmail.com>
2023-03-06 15:04:58 -0800

    commit at 2023-03-06_15:04:58

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 * print_chessboard - locates a substring
 * @a: array
@@ -14,6 +13,6 @@ for (j = 0; j < 8; j++)
 {
 _putchar(a[i][j]);
 }
-_putchar("\n");
+_putchar('\n');
 }
 }
