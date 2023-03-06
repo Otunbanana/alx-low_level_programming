6d756188711745cc163a1acd165ec76054c35a32
Musa Otun <otunmusa@gmail.com>
2023-03-06 15:14:36 -0800

    commit at 2023-03-06_15:14:36

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,18 +8,14 @@
 
 void print_diagsums(int *a, int size)
 {
-int i, j, sum1 = 0, sum2 = 0;
-
-for (i = 0; i < size; i++)
+int i, sum1 = 0, sum2 = 0;
+for (i = 0; i <= size * size; i = i + size + 1)
 {
-sum1 += *(a + i * size + i);
+sum1 = sum1 + *(a + i);
 }
-
-for (i = 0, j = size - 1; i < size; i++, j--)
+for (i = size - 1; i < (size * size - 1); i = i + size - 1)
 {
-sum2 += *(a + i * size + j);
+sum2 = sum2 + *(a + i);
 }
-
-printf("Diagonal sum 1: %d\n", sum1);
-printf("Diagonal sum 2: %d\n", sum2);
+printf("%d, %d\n", sum1, sum2);
 }
