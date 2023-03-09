0733ba1b29eab9ec1bf5e8da34095329a42096a3
Musa Otun <otunmusa@gmail.com>
2023-03-09 00:00:24 -0800

    commit at 2023-03-09_00:00:24

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,18 +12,9 @@ if (n < 0)
 {
 return (-1);
 }
-else if (n == 0)
+if (n == 0)
 {
 return (1);
 }
-else
-{
-int result = 1;
-int i;
-for (i = 1; i <= n; ++i)
-{
-result *= i;
-}
-return (result);
-}
+return (n * factorial(n - 1));
 }
