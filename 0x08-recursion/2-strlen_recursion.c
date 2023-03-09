78a5ad4feb418de01e8eb977e05e951c0641b4ca
Musa Otun <otunmusa@gmail.com>
2023-03-08 20:51:25 -0800

    commit at 2023-03-08_20:51:25

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* _strlen_recursion -  returns the length of a string.
-* @s: string tocount
-* return: 0
+* _strlen_recursion -  returns the length of a string
+* @s: input
+* return: string
 */
 
 int _strlen_recursion(char *s)
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,11 +11,12 @@ int factorial(int n)
 if (n < 0)
 {
 return (-1);
-} 
+}
 else if (n == 0)
 {
 return (1);
-} else
+}
+else
 {
 int result = 1;
 int i;
