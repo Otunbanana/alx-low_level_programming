9ecc3cbe5317f488742afcd9ff731ab0b8f7c46d
Musa Otun <otunmusa@gmail.com>
2023-03-09 04:55:42 -0800

    commit at 2023-03-09_04:55:42

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,37 @@
 #include "main.h"
 /**
-* is_prime_number - returns 1 if the input integer is a prime
+* is_prime_helper - returns 1 if the input integer is a prime
 * @n: base number
+* @i: current value of i being checked
 * Return: boolean
 **/
 
-int is_prime_number(int n)
+int is_prime_helper(int n, int i)
 {
-int i;
 if (n <= 1)
 {
 return (0);
 }
-for (i = 2; i * i <= n; i++)
+else if (i * i > n)
 {
-if (n % i == 0)
+return (1);
+}
+else if (n % i == 0)
 {
 return (0);
 }
+else
+{
+return is_prime_helper(n, i + 1);
 }
-return (1);
+}
+
+/**
+* is_prime_number - returns 1 if the input integer is a prime
+* @n: base number
+* Return: boolean
+**/
+int is_prime_number(int n)
+{
+return is_prime_helper(n, 2);
 }
