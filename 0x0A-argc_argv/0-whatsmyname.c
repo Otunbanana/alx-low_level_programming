2dc522897a2089e46ca83535944809d72527cc3c
Musa Otun <otunmusa@gmail.com>
2023-03-10 17:17:39 -0800

    commit at 2023-03-10_17:17:39

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+* main - Prints its name
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: Always zero
+*/
 
 int main(int argc, char *argv[])
 {
