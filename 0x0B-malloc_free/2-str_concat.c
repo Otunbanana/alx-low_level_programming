a104a79661ffe40c0d76fd941d65ccfb02cb13e6
Musa Otun <otunmusa@gmail.com>
2023-03-14 12:41:15 -0700

    commit at 2023-03-14_12:41:15

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,8 @@
 char *str_concat(char *s1, char *s2)
 {
 char *concat;
+size_t s1_len;
+size_t s2_len;
 if (s1 == NULL)
 {
 s1 = "";
@@ -22,8 +24,8 @@ if (s2 == NULL)
 s2 = "";
 }
 
-size_t s1_len = strlen(s1);
-size_t s2_len = strlen(s2);
+s1_len = strlen(s1);
+s2_len = strlen(s2);
 
 concat = malloc(s1_len + s2_len + 1);
 
