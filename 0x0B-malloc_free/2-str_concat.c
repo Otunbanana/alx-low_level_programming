ea9cde24b1cf7712be8a5af8254fe09036f32be6
Musa Otun <otunmusa@gmail.com>
2023-03-14 12:39:19 -0700

    commit at 2023-03-14_12:39:19

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* *str_concat - concatenates two strings
+* @s1: string one
+* @s2: string two
+*
+* Return: Always zero
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+char *concat;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+
+size_t s1_len = strlen(s1);
+size_t s2_len = strlen(s2);
+
+concat = malloc(s1_len + s2_len + 1);
+
+if (concat == NULL)
+{
+return (NULL);
+}
+
+strcpy(concat, s1);
+strcat(concat, s2);
+
+return (concat);
+}
