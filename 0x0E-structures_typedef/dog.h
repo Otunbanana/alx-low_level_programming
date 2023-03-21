3fe98385fed89e93175260d924a34a986192dcc4
Musa Otun <otunmusa@gmail.com>
2023-03-21 12:46:28 -0700

    commit at 2023-03-21_12:46:28

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,6 +8,7 @@
 *
 * Return: Always 0.
 */
+typedef struct dog dog_t;
 struct dog
 {
 char *name;
@@ -16,4 +17,7 @@ char *owner;
 }
 my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
