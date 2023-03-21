7db1eb2fac06041bbe3eba9c99f76557e2953894
Musa Otun <otunmusa@gmail.com>
2023-03-21 14:49:46 -0700

    commit at 2023-03-21_14:49:46

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,21 +9,12 @@ void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
-if (d->name == NULL)
-{
-printf("Name: (nil)\n");
-}
-else
-{
-printf("Name: %s\n", d->name);
-}
+
+(d->name == NULL) ? printf("Name: (nil)\n")
+: printf("Name: %s\n", d->name);
+
 printf("Age: %f\n", d->age);
-if (d->owner == NULL)
-{
-printf("Owner: (nil)\n");
-}
-else
-{
-printf("Owner: %s\n", d->owner);
-}
+
+(d->owner == NULL) ? printf("Owner: (nil)\n")
+: printf("Owner: %s\n", d->owner);
 }
