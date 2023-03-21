62bfa36831f53e9ce7cf0de84ad4794071e302e2
Musa Otun <otunmusa@gmail.com>
2023-03-21 11:12:03 -0700

    commit at 2023-03-21_11:12:03

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+* struct dog - struct for dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+*
+* Return: Always 0.
+*/
 struct dog
 {
 char *name;
