7557a43e0c1a6d281c5552e28034d8bca4c1bf0c
Musa Otun <otunmusa@gmail.com>
2023-03-21 11:44:42 -0700

    commit at 2023-03-21_11:44:42

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+* init_dog - initialize a variable of type struct dog
+* @d: pointer for my_dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+*
+* Return: Always 0.
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,5 @@ float age;
 char *owner;
 }
 my_dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
