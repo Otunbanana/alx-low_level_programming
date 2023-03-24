edf333b44e87df38940d684931a49e9a65166b31
Musa Otun <otunmusa@gmail.com>
2023-03-23 21:59:45 -0700

    commit at 2023-03-23_21:59:45

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+* main - performs simple operations
+* @argc: first argument 
+* @argv: second argument 
+*
+* Return: always return 0
+*/
+int main(int argc, char *argv[])
+{
+int (*oprt)(int, int);
+
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+
+oprt = get_op_func(argv[2]);
+
+
+if (!oprt)
+{
+printf("Error\n");
+exit(99);
+}
+
+
+printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+return (0);
+}
+
