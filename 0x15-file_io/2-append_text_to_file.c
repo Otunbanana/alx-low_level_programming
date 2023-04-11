d369ec8e407fa036bd9d3105b3d0e76415ab0c79
Musa Otun <otunmusa@gmail.com>
2023-04-10 19:11:33 -0700

    commit at 2023-04-10_19:11:33

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+FILE *file;
+if (filename == NULL)
+return (-1);
+
+file = fopen(filename, "a");
+if (file == NULL)
+return (-1);
+
+if (text_content != NULL)
+fprintf(file, "%s", text_content);
+
+fclose(file);
+return (1);
+}
+
