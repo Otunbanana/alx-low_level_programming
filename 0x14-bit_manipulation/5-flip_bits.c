e28d20af981a46ba3d36cc2c7b0e6e7e028c56d8
Musa Otun <otunmusa@gmail.com>
2023-04-12 12:48:38 -0700

    commit at 2023-04-12_12:48:38

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* flip_bits - Returns the number of bits you would need to flip to 
+* flip_bits - Returns the number of bits you would need to flip to
 * get from one number to another.
 * @n: First number.
 * @m: Second number.
