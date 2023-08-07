7b61fbcd2b105138d5fa00927b4100b932c07b0f
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2023-08-07 21:52:49 +0100

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@
 *
 * Return: 1 on success, -1 on failure
 */
-/* choke on this kimba */ 
+/* choke on this kimba */
 int create_file(const char *filename, char *text_content)
 {
 int fd, len, bytes_written;
