5e3b4f972d4dffdec395d859b06d40991e991648
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-11 21:18:27 +0100

    0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* print_dlistint - Prints all elements of a dlistint_t list
+* @h: Pointer to the head of the list
+*
+* Description: This function traverses a doubly linked list of type dlistint_t,
+*              printing the value of each node. It starts from the given head
+*              pointer and continues until the end of the list, counting and
+*              printing each element.
+*
+* Return: The number of nodes in the list
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+
+return (count);
+}
