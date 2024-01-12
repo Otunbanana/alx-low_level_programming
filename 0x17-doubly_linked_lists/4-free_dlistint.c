04dbd5a4541523daef902d650a71dd2b707bb6da
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-12 21:41:12 +0100

    4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,39 +1,17 @@
 #include "lists.h"
 
 /**
-* add_dnodeint_end - Adds a new node at the end of a dlistint_t list
+* free_dlistint - Frees a dlistint_t list
 * @head: Pointer to the head of the list
-* @n: Value to be assigned to the new node
-*
-* Return: The address of the new element, or NULL if it failed
 */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+void free_dlistint(dlistint_t *head)
 {
-dlistint_t *new_node, *temp;
+dlistint_t *temp;
 
-if (head == NULL)
-return (NULL);
-
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
-
-new_node->n = n;
-new_node->next = NULL;
-
-if (*head == NULL)
+while (head != NULL)
 {
-new_node->prev = NULL;
-*head = new_node;
-return (new_node);
+temp = head;
+head = head->next;
+free(temp);
 }
-
-temp = *head;
-while (temp->next != NULL)
-temp = temp->next;
-
-temp->next = new_node;
-new_node->prev = temp;
-
-return (new_node);
 }
